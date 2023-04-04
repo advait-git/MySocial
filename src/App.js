de145da8b7bf7b66a3c87a1acfb3f6ce9b6b36e8
advait-git <advaittiwari2@gmail.com>
2023-04-04 14:42:12 +0530

    new things

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from "./post";
 function App() {
+  const [posts,setPost] = useState([
+    {
+      username: "RakaZoneGaming",
+      caption:"wow it works",
+      imageUrl:"https://www.indiagamingaward.com/assets/2022/images/jury-categories/Streamer-Of-The-Year-Male/Rishabh-Rakazone-Gaming-Karanwal.jpg"
+    },
+    {
+      username: "Shroud",
+      caption:"wow it works",
+      imageUrl:"https://th.bing.com/th/id/OIP.enYcXzNWe_diI9_Af09Q0QHaEK?w=318&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    }
+  ]);
   return (
     <div className="App">
       <div className="app_headerImage">
@@ -10,11 +22,13 @@ function App() {
       src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
       alt=""
         />
-
       </div>
-      <Post username='RakaZoneGaming' caption='wow it works' imageUrl='https://www.indiagamingaward.com/assets/2022/images/jury-categories/Streamer-Of-The-Year-Male/Rishabh-Rakazone-Gaming-Karanwal.jpg' />
-      <Post />
-      <Post />
+      {
+        posts.map(post =>(
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+      
     </div>
   );
 }
--- src/post.js
+++ src/post.js
@@ -2,21 +2,21 @@ import React from 'react';
 import "./post.css";
 import { Avatar } from '@mui/material';
 
-function post({ username ,caption ,imageUrl }) {
+function post({ username , caption , imageUrl }) {
   return (
     <div className="post">
       <div className='post_header'>
       <Avatar 
       className="post_avatar"
-      alt="Shroud"
-      src='https://mir-s3-cdn-cf.behance.net/project_modules/1400/eb46f666181241.5b0d977d9e971.jpg'/>
+      alt=" "
+      src=''/>
       <h3>{username}</h3>
       </div>
 
-      <img className="post_image" src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt="" />
+      <img className= "post_image" src= {imageUrl} alt= " " />
 
 
-      <h4 className="post_text"><strong>Advait</strong>  you are doing good!</h4>
+      <h4 className="post_text"> <strong> {username} </strong> {caption} </h4>
     </div>
   );
 }
